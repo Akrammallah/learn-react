43ea590739535597d5b34cb9962924043b1b4bdf
Akrammallah <153257896+Akrammallah@users.noreply.github.com>
2024-06-13 12:19:35 +0500

    passwordgeneratorproject

--- bgcolorchangerproject/src/App.jsx
+++ bgcolorchangerproject/src/App.jsx
@@ -12,22 +12,28 @@ function App() {
       
      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
       <div className='fixed flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-        <button onClick={() => setColor("red")} 
+        <button 
+        onClick={() => setColor("red")} 
         className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
         style={{backgroundColor:"red"}}>Red</button>
-        <button onClick={() => setColor("black")} 
+        <button 
+        onClick={() => setColor("black")} 
         className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
         style={{backgroundColor:"black"}}>black</button>
-        <button onClick={() => setColor("green")} 
+        <button 
+        onClick={() => setColor("green")} 
         className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
         style={{backgroundColor:"green"}}>green</button>
-        <button onClick={() => setColor("grey")}  
+        <button 
+        onClick={() => setColor("grey")}  
         className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
         style={{backgroundColor:"grey"}}>grey</button>
-        <button onClick={() => setColor("blue")} 
+        <button 
+        onClick={() => setColor("blue")} 
         className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
         style={{backgroundColor:"blue"}}>blue</button>
-        <button onClick={() => setColor("yellow")} 
+        <button 
+        onClick={() => setColor("yellow")} 
         className='outline-none px-4 py-1 rounded-full text-black shadow-lg'
         style={{backgroundColor:"yellow"}}>yellow</button>
         <button 
